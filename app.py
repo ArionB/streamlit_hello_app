a3f62b330d40761ff62b92e9fbd4e41770278fde
ArionB <abaqaj.ab@gmail.com>
2022-11-26 17:16:57 +0100

    time added

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title('hello streamlit app')
 st.header('This is added after deployment')
+from datetime import datetime, time
+date_now = datetime.now
+st.write(time_now)
